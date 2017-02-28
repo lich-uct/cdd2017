28a146f7414280bfe5285e11c6682d500e450b09
Ivan Cmelo <ivan@cmelo.cz>
2017-02-28 14:10:54 +0100

    Added conda environment YML file. Looks ugly, but should work. Also added a little convenience script for running it.

---
+++ environment/run_conda_env_create.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+### A little convenience script for running the environment installation.
+
+# Just run the conda installer for now:
+conda env create -f conda_environment.yml
+
+# Might be extended later
